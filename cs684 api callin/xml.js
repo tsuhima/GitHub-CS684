39ca76e1a85df428dd3e61eaba45cc299b47a031
TanishBugnait29 <81473377+TanishBugnait29@users.noreply.github.com>
2022-02-01 10:51:43 -0500

    Update xml.js

--- cs684 api callin/xml.js
+++ cs684 api callin/xml.js
@@ -1,11 +1,7 @@
 function setAction() {
-
-
-
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", 'https://cs684api.7g2rji63c728k.us-east-1.cs.amazonlightsail.com/hw3?team='+document.getElementById("team").value, false ); // false for synchronous request
     xmlHttp.send( null ); // The null parameter indicates that no body content is needed for the GET request
-    
     document.getElementById("id1").value = xmlHttp.responseText 
   return false;
 }
